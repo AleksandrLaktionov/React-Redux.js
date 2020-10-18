ce59c02078bd654f3694f3a12678d2564d468720
AleksandrLaktionov <72191399+AleksandrLaktionov@users.noreply.github.com>
2020-10-18 15:14:45 +0300

    Updates

---
+++ precache-manifest.9dd9ea2ac57f8ca627fd5ea2dde158f6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64be11658a6e192ff450582fd477dd43",
+    "url": "/React-Redux.js/index.html"
+  },
+  {
+    "revision": "c75dce6e1ecf18bf16d2",
+    "url": "/React-Redux.js/static/css/3.4cd9a8a5.chunk.css"
+  },
+  {
+    "revision": "be488019741f069a5437",
+    "url": "/React-Redux.js/static/css/4.47035ed0.chunk.css"
+  },
+  {
+    "revision": "24ed5fbc63a330587d1d",
+    "url": "/React-Redux.js/static/css/5.eaf5c1ad.chunk.css"
+  },
+  {
+    "revision": "37aea67469175d66e977",
+    "url": "/React-Redux.js/static/css/main.e1591712.chunk.css"
+  },
+  {
+    "revision": "ac9baf03bc21f9715a9a",
+    "url": "/React-Redux.js/static/js/2.59e68bd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux.js/static/js/2.59e68bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75dce6e1ecf18bf16d2",
+    "url": "/React-Redux.js/static/js/3.9fd5768a.chunk.js"
+  },
+  {
+    "revision": "be488019741f069a5437",
+    "url": "/React-Redux.js/static/js/4.a8dc4259.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/React-Redux.js/static/js/4.a8dc4259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ed5fbc63a330587d1d",
+    "url": "/React-Redux.js/static/js/5.baa637fa.chunk.js"
+  },
+  {
+    "revision": "37aea67469175d66e977",
+    "url": "/React-Redux.js/static/js/main.a02be128.chunk.js"
+  },
+  {
+    "revision": "f60c685ef11b4d0b2e92",
+    "url": "/React-Redux.js/static/js/runtime-main.540cdccb.js"
+  },
+  {
+    "revision": "e75c6bd1c90a05dba54bf345cbe60c00",
+    "url": "/React-Redux.js/static/media/Lucky Luke - Cooler Than Me.e75c6bd1.mp3"
+  },
+  {
+    "revision": "b3dea5eb3036df8160e7bffc06dae518",
+    "url": "/React-Redux.js/static/media/blue-butterfly.b3dea5eb.jpg"
+  },
+  {
+    "revision": "601aa9486e512d98d022ffc666653761",
+    "url": "/React-Redux.js/static/media/loading.601aa948.svg"
+  }
+]);
